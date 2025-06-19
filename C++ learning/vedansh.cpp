62d395bff79cfca216bc34ef4e63ae7dee78b688
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-20 01:23:46 +0530

    rakesh

--- C++ learning/vedansh.cpp
+++ C++ learning/vedansh.cpp
@@ -2,14 +2,30 @@
 using namespace std;
 
 int main () {
-    cout<<"Vedansh\nMathur\n";
-    int age = 18;
-    char grade = 'A';
-    float num = 145.98f;
+    // cout<<"Vedansh\nMathur\n";
+    // int age = 18;
+    // char grade = 'A';
+    // float num = 145.98f;
 
-    cout<<age<<endl;
-    cout<<grade<<endl;
-    cout<<num<<endl;
-    cout<<sizeof(age);
+    // cout<<age<<endl;
+    // cout<<grade<<endl;
+    // cout<<num<<endl;
+    // cout<<sizeof(age);
+
+    // char grade = 'A';
+    // int value = grade;
+    // cout<<sizeof(value)<<endl;
+    // cout<<sizeof(grade);  
+// big to small, easily
+
+    // double op = 265.89;
+    // int price = (int)op;
+    // cout<<sizeof(price)<<endl;
+    // cout<<sizeof(op)<<endl;  // to assign big to small
+
+    int age ;
+    cout<< "Enter your name : \n";
+    cin>>age;
+    cout<< "Your name is : "<<age;
     return 0;
 }
