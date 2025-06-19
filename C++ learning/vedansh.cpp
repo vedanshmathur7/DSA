9f3e527d09c689d41acc04da6d4fee6815453ee4
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-20 01:13:42 +0530

    rajesh

--- C++ learning/vedansh.cpp
+++ C++ learning/vedansh.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int main () {
     cout<<"Vedansh\nMathur\n";
     int age = 18;
+    char grade = 'A';
+    float num = 145.98f;
+
     cout<<age<<endl;
+    cout<<grade<<endl;
+    cout<<num<<endl;
     cout<<sizeof(age);
     return 0;
 }
