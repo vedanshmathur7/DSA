5b3ff0a6f899650dd9d4abd9ebfb0d6910dd6b93
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-20 01:36:00 +0530

    op

--- C++ learning/vedansh.cpp
+++ C++ learning/vedansh.cpp
@@ -23,9 +23,37 @@ int main () {
     // cout<<sizeof(price)<<endl;
     // cout<<sizeof(op)<<endl;  // to assign big to small
 
-    int age ;
-    cout<< "Enter your name : \n";
-    cin>>age;
-    cout<< "Your name is : "<<age;
+    // int age ;
+    // cout<< "Enter your name : \n";
+    // cin>>age;
+    // cout<< "Your name is : "<<age;
+
+    // cout << 5/2<< endl;
+    // cout << 5%2<< endl;
+    // cout << 5+2<< endl;
+    // cout << 5-2<< endl;
+    // cout << (5==2)<< endl;
+    // cout << (5==5)<< endl;
+    // cout << (5<=2 || 4 <=7)<< endl;
+    // cout << (5<=2 && 4 <=7)<< endl;
+
+    // int a , b;
+    // cout << "Enter the num 'a' : \n";
+    // cin >> a;
+    // cout << "Enter the num 'b' : \n";
+    // cin >> b;
+    // cout<<"The sum of a and b is : "<< (a+b);
+
+    // int a = 6;
+    // cout<<++a;
+    // cout<<a++;
+    // cout<<a;
+    // cout<<a--;
+    // cout<<a;
+
+    int a = 45;
+    if (a<= 90){
+        cout<< "The no. is "
+    }
     return 0;
 }
