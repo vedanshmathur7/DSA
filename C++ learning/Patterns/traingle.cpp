2880dd91a9edb7b99841083d9e737efec4aa228c
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-27 02:43:19 +0530

    traingle pattern with no.

--- C++ learning/Patterns/traingle.cpp
+++ C++ learning/Patterns/traingle.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 int main () {
-    int n = 10;
+    // int n = 10;
+    // for (int i = 1; i <=n; i++){
+    //     for (int j = 1; j <=i; j++){
+    //         cout<<" *";
+    //     }
+    // cout<<endl;
+    // }
+
+    int n = 9;
     for (int i = 1; i <=n; i++){
         for (int j = 1; j <=i; j++){
-            cout<<" *";
+            cout<<i;
         }
     cout<<endl;
     }
