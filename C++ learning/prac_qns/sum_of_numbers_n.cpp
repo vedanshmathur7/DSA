9742a3cd09171a6db834555f6c2275202bc89563
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-26 23:39:05 +0530

    break

--- C++ learning/prac_qns/sum_of_numbers_n.cpp
+++ C++ learning/prac_qns/sum_of_numbers_n.cpp
@@ -6,8 +6,23 @@ int main (){
 
     for (int i = 1; i<=n; i++){
         sum += i;
-        
+        if (i == 2){
+            continue;
+        }   
     }
     cout<<sum;
+   
+   
+   
+   
+   
+    // int i = 1 ;
+    // while (i <= n){
+    //     sum += i;
+    //     i++;
+    // }
+    // cout<<sum;
+
+
     return 0;
 }
