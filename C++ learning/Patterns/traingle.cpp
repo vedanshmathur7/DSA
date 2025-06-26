b28cae59f7eef215080d6492c6b2519a03f08233
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-27 02:58:06 +0530

    traingle pattern with alphabets

--- C++ learning/Patterns/traingle.cpp
+++ C++ learning/Patterns/traingle.cpp
@@ -10,10 +10,20 @@ int main () {
     // cout<<endl;
     // }
 
+    // int n = 9;
+    // for (int i = 1; i <=n; i++){
+    //     for (int j = 1; j <=i; j++){
+    //         cout<<i<<' ';
+    //     }
+    // cout<<endl;
+    // }
+
     int n = 9;
-    for (int i = 1; i <=n; i++){
-        for (int j = 1; j <=i; j++){
-            cout<<i;
+   
+    for (int i = 0; i <n; i++){
+    char op = 'A' + i;
+        for (int j = 0; j <=i; j++){
+            cout<<op<<' ';
         }
     cout<<endl;
     }
