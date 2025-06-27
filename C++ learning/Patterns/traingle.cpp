445cbd5288ae60725e8e255d5fbaed92b5952982
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-27 14:18:03 +0530

    triangle pattern numeric

--- C++ learning/Patterns/traingle.cpp
+++ C++ learning/Patterns/traingle.cpp
@@ -18,14 +18,27 @@ int main () {
     // cout<<endl;
     // }
 
-    int n = 9;
+    // int n = 9;
    
-    for (int i = 0; i <n; i++){
-    char op = 'A' + i;
-        for (int j = 0; j <=i; j++){
-            cout<<op<<' ';
+    // for (int i = 0; i <n; i++){
+    // char op = 'A' + i;
+    //     for (int j = 0; j <=i; j++){
+    //         cout<<op<<' ';
+    //     }
+    // cout<<endl;
+    // }
+
+
+    int n = 6;
+
+    for (int i = 1; i <=n; i++){
+        for (int j = 1; j<=i; j++){
+            cout<<j<<' ';
         }
-    cout<<endl;
+        cout<<endl;
     }
+
+
+
     return 0;
 }
