1c3ed44c683236e320ef91946dcbe2cc0974c676
Vedansh Mathur <vedanshmathur007@gmail.com>
2025-06-28 15:03:33 +0530

    floyd's triangle pattern with alphabets

--- C++ learning/Patterns/floyds_triangle.cpp
+++ C++ learning/Patterns/floyds_triangle.cpp
@@ -2,15 +2,26 @@
 using namespace std;
 
 int main () {
-    int n = 4;
-    int num = 1;
-    for (int i = 1; i <=n; i++){
-        for (int j = 1; j<=i; j++){
-            cout<<num<<' ';
-            num++;
-        }
-        cout<<endl;
+    // int n = 4;
+    // int num = 1;
+    // for (int i = 1; i <=n; i++){
+    //     for (int j = 1; j<=i; j++){
+    //         cout<<num<<' ';
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
 
+
+    int n = 5;
+    char op = 'A';
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1 ; j<=i; j++){
+            cout<<op<<' ';
+            op++;
+        }
+        cout <<endl;
     }
     return 0;
 }
