2fdb19e323925bf407a7db50a1b8e0c8342d02fb
vedanshmathur7 <vedanshmathur007@gmail.com>
2025-08-16 20:30:31 +0530

    Create linear_search.cpp

---
+++ Arrays/linear_search.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch (int arr[], int size, int target){
+    for (int i =0; i<size; i++){
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main () {
+    
+    int arr[] = {4, 58, 2, 56, 14};
+    int size = 5;
+    int target = 14;
+
+    cout<<linearSearch(arr, size, target)<<endl;
+
+    return 0;
+}
