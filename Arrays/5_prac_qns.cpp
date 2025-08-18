ee6aa5f53408c58e1696ded1bab4dd8c500c7330
vedanshmathur7 <vedanshmathur007@gmail.com>
2025-08-19 01:46:30 +0530

    Update 5_prac_qns.cpp

--- Arrays/5_prac_qns.cpp
+++ Arrays/5_prac_qns.cpp
@@ -66,38 +66,69 @@
 
 // -----------------Q3 (to print all the unique values of the array)---------------------
 
+// #include <iostream>
+// using namespace std;
+
+// void findUnique (int arr[], int size){
+
+
+//     for (int i =0; i<size; i++){
+//         bool isunique= true;
+
+//         for (int j=0; j<size; j++){
+//             if (i!=j && arr[i] == arr[j]){
+//                 isunique = false;
+//                 break;
+//             }
+//         }
+
+//         if (isunique){
+//             cout<<arr[i]<<endl;
+//         }
+//     }
+//     cout<<endl;
+
+// }
+
+// int main () {
+    
+//     // int arr[] = {89, 89, 58, 56, 58, 56, 25, 25, 5, 9, 90, 87};
+//     int arr[] = {10, 20, 30, 20, 40, 10, 50};
+
+//     int size = 7;
+
+//     findUnique(arr, size);
+
+//     return 0;
+// }
+
+
+
+// -----------------------------Q4 (to print intersection of two arrays)-----------------
+
 #include <iostream>
 using namespace std;
 
-void findUnique (int arr[], int size){
-
-
-    for (int i =0; i<size; i++){
-        bool isunique= true;
-
-        for (int j=0; j<size; j++){
-            if (i!=j && arr[i] == arr[j]){
-                isunique = false;
+void intersection (int arr1[], int arr2[], int size1, int size2){
+    for (int i =0; i<size1; i++){
+        for (int j=0; j<size2; j++){
+            if (arr1[i] == arr2[j]){
+                cout<<arr1[i]<<" ";
                 break;
             }
         }
-
-        if (isunique){
-            cout<<arr[i]<<endl;
-        }
     }
     cout<<endl;
-
 }
 
 int main () {
     
-    // int arr[] = {89, 89, 58, 56, 58, 56, 25, 25, 5, 9, 90, 87};
-    int arr[] = {10, 20, 30, 20, 40, 10, 50};
+    int arr1[] = {23, 25, 65, 58, 78, 67};
+    int arr2[] = {23, 25, 65, 58, 78, 56, 90};
+    int size1 = sizeof(arr1)/sizeof(arr1[0]);
+    int size2 = sizeof(arr2)/sizeof(arr2[0]);
 
-    int size = 7;
-
-    findUnique(arr, size);
+    intersection(arr1, arr2, size1, size2);
 
     return 0;
 }
